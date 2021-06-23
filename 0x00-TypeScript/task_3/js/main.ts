9a114f9b2bf1b09836b715cd6d0ae778f0fb8f76
747-diego <725@holbertonschool.com>
2021-06-23 02:33:45 -0400

    [TESTING LINTER]

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,4 +1,4 @@
-/// <reference path="./crud.d.ts" />
+/// <reference path = "./crud.d.ts" />
 
 import { RowID } from './interface';
 import { RowElement } from './interface';
@@ -11,6 +11,5 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row)
 const updatedRow: RowElement = { ...row, age: 23 };
-
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
