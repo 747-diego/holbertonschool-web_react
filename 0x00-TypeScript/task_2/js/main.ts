f17b9b1ab604425fa42e6c0b5d4a469619a5d649
747-diego <725@holbertonschool.com>
2021-06-22 23:14:59 -0400

    [TASK ADVANCED TYPES]

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -10,19 +10,19 @@ interface TeacherInterface {
 	workTeacherTasks(): string;
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
 	workFromHome() { return 'Working from home'; }
 	getCoffeeBreak() { return 'Getting a coffee break'; }
 	workDirectorTasks() { return 'Getting to director tasks'; }
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
 	workFromHome() { return 'Cannot work from home'; }
 	getCoffeeBreak() { return 'Cannot have a break'; }
 	workTeacherTasks() { return 'Getting to work'; }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
+export function createEmployee(salary: number | string): Director | Teacher {
 	if (typeof salary === 'number' && salary < 500) return new Teacher();
 	return new Director();
 }
