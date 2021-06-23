bc9e859be6549cff0d579280fb1f70e68788740b
747-diego <725@holbertonschool.com>
2021-06-22 23:38:51 -0400

    [TASK 6. FUNCTIONS FOR EMPLOYEES]

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -41,6 +41,33 @@ export class Teacher implements TeacherInterface {
 }
 
 export function createEmployee(salary: number | string): Director | Teacher {
-	if (typeof salary === 'number' && salary < 500) return (new Teacher());
-	return (new Director());
+	if (typeof salary === 'number' && salary < 500) {
+        return (new Teacher());
+    }
+	else {
+        return (new Director());
+    }
+}
+
+export function isDirector(employee: Director | Teacher): employee is Director {
+	return (employee instanceof Director);
+}
+
+export function executeWork(employee: Director | Teacher): string {
+	if (isDirector(employee)) {
+        return (employee.workDirectorTasks());
+    }
+	else {
+        return (employee.workTeacherTasks());
+    }
+}
+
+export type Subjects = 'Math' | 'History';
+export function teachClass(todayClass: Subjects): string {
+	if (todayClass === 'Math') {
+        return ('Teaching Math');
+    }
+	if (todayClass === 'History') {
+        return ('Teaching History');
+    }
 }
