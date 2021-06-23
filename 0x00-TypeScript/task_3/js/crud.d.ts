dbc8cd08422ba33012aabd1fcbdf6be7b6a075cc
747-diego <725@holbertonschool.com>
2021-06-23 02:52:54 -0400

    [TESTING LINTER]

--- 0x00-TypeScript/task_3/js/crud.d.ts
+++ 0x00-TypeScript/task_3/js/crud.d.ts
@@ -1,4 +1,5 @@
-import { RowID, RowElement } from './interface';
+import { RowID } from './interface';
+import { RowElement } from './interface';
 
 declare function insertRow(row: RowElement): number;
 declare function deleteRow(rowId: RowID): void;
--- 0x00-TypeScript/task_3/js/interface.ts
+++ 0x00-TypeScript/task_3/js/interface.ts
@@ -1,6 +1,6 @@
 export type RowID = number;
 
-exported interface RowElement {
+export interface RowElement {
 	firstName: string;
 	lastName: string;
 	age?: number;
