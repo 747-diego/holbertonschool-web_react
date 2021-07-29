77687f91073f2eb4d269390adc53427785cb2dbb
747-diego <725@holbertonschool.com>
2021-07-28 20:55:16 -0400

    [TASK 4. CHAINED MUTATIONS]

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -6,7 +6,7 @@ const map = Map({
   3: 'Elijah',
   4: 'Oliver',
   5: 'Jacob',
-  6: 'Lucas',
+  6: 'Lucas'
 });
 const map2 = List(map).withMutations((list) => {
   list.set(2, 'Benjamin').set(4, 'Oliver');
