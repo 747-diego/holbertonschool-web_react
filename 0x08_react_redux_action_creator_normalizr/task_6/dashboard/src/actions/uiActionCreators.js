2090cc6d80cbaaf6bee10c02db5ca669756dc258
747-diego <725@holbertonschool.com>
2021-08-04 23:24:25 -0400

    [ADDING NEW FILES]

---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,17 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+export function selectCourse(index) {
+  const sel = {
+    type: SELECT_COURSE,
+    index,
+  };
+  return (sel);
+}
+
+export function unSelectCourse(index) {
+  const uns = {
+    type: UNSELECT_COURSE,
+    index,
+  };
+  return (uns);
+}
